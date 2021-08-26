8ad2bce41c085e3ea24c32afae7153f7cdc11bf3
PerfectFixes <ttay29@gmail.com>
2021-08-27 02:34:07 +0300

    Map Update

---
+++ Chaos Hero/Assets/Scripts/PlayerEngine.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEngine : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    float xPos;
+    float speed = 7f;
+    int jumpCounter = 0;
+    void Start()
+    {
+        //Starting position set
+      
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        xPos = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(xPos, 0, 0);
+
+        if (Input.GetKeyDown("space") && jumpCounter == 0)
+        {
+            jumpCounter++;
+            playerRB.AddForce(new Vector2(0, 370));
+        }
+        else if (Input.GetKeyDown("space") && jumpCounter == 1)
+        {
+            jumpCounter++;
+            playerRB.AddForce(new Vector2(0, 150));
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            jumpCounter = 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Lava")
+        {
+            transform.position = new Vector2(-110, -2.5f);
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}

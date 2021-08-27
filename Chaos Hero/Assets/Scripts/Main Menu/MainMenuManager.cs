c829f2d70c1d2f6333480cdb328529fa5ebade42
PerfectFixes <ttay29@gmail.com>
2021-08-27 20:55:19 +0300

    Added the Camera Flip and fixed a bit of the map

---
+++ Chaos Hero/Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void ChangeScene()
+    {
+        //Load the level as you left it
+        SceneManager.LoadScene("The Level");
+    }
+    public void NewGame()
+    {
+        //Resetting all of the playerprefs to start a frash new game
+        PlayerPrefs.SetString("DJCancel", "False");
+        PlayerPrefs.SetString("Bad Platform", "False");
+        PlayerPrefs.SetString("Adding Enemies", "False");
+        PlayerPrefs.SetString("Better Enemies", "False");
+        PlayerPrefs.SetString("Raining meteores", "False");
+        PlayerPrefs.SetString("Timer On", "False");
+        PlayerPrefs.SetString("Raising Lava", "False");
+        PlayerPrefs.SetString("Invert Controls", "Flase");
+        PlayerPrefs.SetString("Flip Map", "False");
+        PlayerPrefs.SetString("Random Kill", "False");
+
+        //Loading the level
+        SceneManager.LoadScene("The Level");
+    }
+    public void QuitGame()
+    {
+        //Quit
+        Application.Quit();
+    }
+}
---
+++ Chaos Hero/Assets/Scripts/Player/FlipCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipCamera : MonoBehaviour
+{
+
+    void Start()
+    {
+        if (PlayerPrefs.GetString("Flip Map") == "True")
+        {
+            print("Flipping the camera");
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+    }
+}

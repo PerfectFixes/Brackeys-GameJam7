702ecb1ce9be135ce1d81d05be6d594004d83a6b
PerfectFixes <ttay29@gmail.com>
2021-08-27 22:06:00 +0300

    Added bad platforms & camera rotation

---
+++ Chaos Hero/Assets/Scripts/Debuffs/BadPlatforms.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadPlatforms : MonoBehaviour
+{
+    private Renderer changeColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeColor = GetComponent<Renderer>();
+        if (PlayerPrefs.GetString("Bad Platform") == "False")
+        {
+            print("Changing Color ");
+
+            changeColor.material.color = Color.red;
+            //Change the tag 
+            transform.gameObject.tag = "Lava";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
